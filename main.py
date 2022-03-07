f4df754a71155ec4225cae22576cff789d4a5397
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-03-07 21:19:55 +0700

    komentar

---
+++ main.py
@@ -0,0 +1,8 @@
+gaji_karyawan = 3000000
+if gaji_karyawan <= 4000000 and gaji_karyawan <= 5000000:
+    print("kinerjanya harus ditambah")
+elif gaji_karyawan > 6000000 and gaji_karyawan <= 7000000:
+    print("belum dapat bonus")
+else:
+    gaji_karyawan <= 8000000
+    print("sudah dapat bonus")
